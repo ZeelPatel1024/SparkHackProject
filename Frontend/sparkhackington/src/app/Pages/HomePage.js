eda74dd57abfd341aeb56fbfc9f812855fbf4e98
Harsh D <102835271+HuskyONG@users.noreply.github.com>
2025-02-08 15:01:13 -0600

    new

--- Frontend/sparkhackington/src/app/Pages/HomePage.js
+++ Frontend/sparkhackington/src/app/Pages/HomePage.js
@@ -42,19 +42,6 @@ export default function Home() {
     }
 
   return (
-<<<<<<< HEAD
-    <div className="home"> 
-            <h1 >Welcome to _______</h1>
-            <button onClick={submitNote}>Click to redirect</button>
-            <button onClick={navigateToSignUpPage}>Login Buisness Account</button>
-            {jobs.map(function fn(job)
-            {
-              return(
-                <h1>{job.name} {job.description} {job.image} {job.available ? <h1>True</h1> : <h1>False</h1>}</h1>
-              );
-            })}
-
-=======
     <div>
       <nav>
       <div class = "nav">
@@ -68,10 +55,9 @@ export default function Home() {
       Welcome to Harsh Dizzler
       <div>
         <button onClick={submitNote}>Click to redirect</button>
-        <button>Create Buisness Account</button>
+        <button  onClick={navigateToSignUpPage} >Create Buisness Account</button>
       </div>
     </header>
->>>>>>> 5b4d04164d3c7174d002626a1f744c706ff891bd
     </div>
   );
 }
