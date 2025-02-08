6dacc82764f40e8a4728db433aaec85a5223bd6b
zpate6 <zpate6@uic.edu>
2025-02-07 21:30:45 -0600

    Initial commit

---
+++ src/main/java/com/example/SparkHackProject/SparkHackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SparkHackProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkHackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkHackProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SparkHackProject/SparkHackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SparkHackProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkHackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
